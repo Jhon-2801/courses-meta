0b6199d69cd4f56e834d773cc452549c01524fe9
Alex <jangulovides@gmail.com>
2023-08-02 20:39:52 -0500

    meta: Agragamos limite de paginas

--- meta/meta.go
+++ meta/meta.go
@@ -1,7 +1,6 @@
 package meta
 
 import (
-	"os"
 	"strconv"
 )
 
@@ -12,16 +11,18 @@ type Meta struct {
 	TotalCount int `json:"total_count"`
 }
 
-func New(page, perPage, total int) (*Meta, error) {
+func New(page, perPage, total int, pagLimDef string) (*Meta, error) {
+
 	if perPage <= 0 {
 		var err error
-		perPage, err = strconv.Atoi(os.Getenv("PAGINATOR_LIMIT_DEFAULT"))
+		perPage, err = strconv.Atoi(pagLimDef)
 		if err != nil {
 			return nil, err
 		}
-	}
-	pageCount := 0
 
+	}
+
+	pageCount := 0
 	if total >= 0 {
 		pageCount = (total + perPage - 1) / perPage
 		if page > pageCount {
@@ -32,17 +33,19 @@ func New(page, perPage, total int) (*Meta, error) {
 	if page < 1 {
 		page = 1
 	}
+
 	return &Meta{
 		Page:       page,
 		PerPage:    perPage,
-		PageCount:  pageCount,
 		TotalCount: total,
+		PageCount:  pageCount,
 	}, nil
 }
 
 func (p *Meta) Offset() int {
 	return (p.Page - 1) * p.PerPage
 }
+
 func (p *Meta) Limit() int {
 	return p.PerPage
 }
